7077cf656f1ec95f6026d8d6c94aef2eb7abc230
Bogdan Nicula <bogdan@nicula.net>
2014-04-23 22:26:00 +0000

    data: hvJP2K - jpx_merge.py: Python 3 compatibility + speedups
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/data/jpeg2000/hvJP2K@1010 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- hvJP2K/jpx/jpx_merge.py
+++ hvJP2K/jpx/jpx_merge.py
@@ -1,11 +1,16 @@
 
 import struct
-from cStringIO import StringIO
+import sys
+
+if sys.hexversion >= 0x03000000:
+    from io import BytesIO
+else:
+    from cStringIO import StringIO as BytesIO
 
 from glymur import Jp2k, jp2box
 
 from ..jp2.jp2_common import first_box, copy_codestream, codestream_size
-import jpx_common
+from . import jpx_common
 
 
 def jpx_merge(names_in, jpxname, links):
@@ -13,14 +18,15 @@ def jpx_merge(names_in, jpxname, links):
     jp2num = len(names_in)
 
     ftbl = jp2box.FragmentTableBox(box=[jp2box.FragmentListBox([1], [1], [1])])
-    asoc = [jp2box.AssociationBox(box=[jp2box.NumberListBox(associations=(0x01000000+i, 0x02000000+i)), None]) for i in range(jp2num)]
+    #asoc = [jp2box.AssociationBox(box=[jp2box.NumberListBox(associations=(0x01000000+i, 0x02000000+i)), None]) for i in range(jp2num)]
+    xmls = [None]*jp2num
 
     # The typical pattern of empty jpch, jplh, e.g.
     # jp2box.CodestreamHeaderBox().write(jpx)
     # jp2box.CompositingLayerHeaderBox().write(jpx)
     jpch_jplh = struct.pack('>I4sI4s', 8, b'jpch', 8, b'jplh')
 
-    jpx = StringIO()
+    jpx = BytesIO()
     jp2box.JPEG2000SignatureBox().write(jpx)
     jp2box.FileTypeBox(brand='jpx ', compatibility_list=('jpx ', 'jp2 ', 'jpxb')).write(jpx)
 
@@ -28,9 +34,10 @@ def jpx_merge(names_in, jpxname, links):
         jp2name = names_in[i]
         jp2 = Jp2k(jp2name)
 
-        jp2h = first_box(jp2, b'jp2h')
-        asoc[i].box[1] = first_box(jp2, b'xml ')
-        jp2c = first_box(jp2, b'jp2c')
+        jp2h = first_box(jp2, 'jp2h')
+        xmls[i] = first_box(jp2, 'xml ')
+        #asoc[i].box[1] = first_box(jp2, 'xml ')
+        jp2c = first_box(jp2, 'jp2c')
 
         with open(jp2name, 'rb') as ifile:
             ifile.seek(jp2h.offset)
@@ -45,14 +52,14 @@ def jpx_merge(names_in, jpxname, links):
                 jpx.write(jpch_jplh)
             else:  # different size/colour spec
                 # write all boxes, could be optimized
-                ihdr = first_box(jp2h, b'ihdr')
-                colr = first_box(jp2h, b'colr')
-                pclr = first_box(jp2h, b'pclr')
-                cmap = first_box(jp2h, b'cmap')
+                ihdr = first_box(jp2h, 'ihdr')
+                colr = first_box(jp2h, 'colr')
+                pclr = first_box(jp2h, 'pclr')
+                cmap = first_box(jp2h, 'cmap')
 
                 if cmap is None:  # create direct colour mapping
                     num = ihdr.num_components
-                    cmap = jp2box.ComponentMappingBox(component_index=range(num),
+                    cmap = jp2box.ComponentMappingBox(component_index=list(range(num)),
                                                       mapping_type=(0,)*num,
                                                       palette_index=(0,)*num)
 
@@ -71,12 +78,38 @@ def jpx_merge(names_in, jpxname, links):
             else:
                 copy_codestream(jp2c, ifile, jpx)
 
-    jp2box.AssociationBox(box=[box for box in asoc if box.box[1] is not None]).write(jpx)
+    #jp2box.AssociationBox(box=[box for box in asoc if box.box[1] is not None]).write(jpx)
+    orig_pos = jpx.tell()
+    jpx.write(struct.pack('>I4s', 0, b'asoc'))
+
+    nlst = jp2box.NumberListBox(associations=[0, 0])
+    asoc = jp2box.AssociationBox(box=[nlst, None])
+    for i in range(jp2num):
+        if xmls[i] is not None:
+            nlst.associations[0] = 0x01000000+i
+            nlst.associations[1] = 0x02000000+i
+            asoc.box[1] = xmls[i]
+            asoc.write(jpx)
+
+    end_pos = jpx.tell()
+    jpx.seek(orig_pos)
+    jpx.write(struct.pack('>I', end_pos - orig_pos))
+    jpx.seek(end_pos)
 
     if links:
-        # I.7.3.2: null terminated
-        url_ = [jp2box.DataEntryURLBox(0, (0, 0, 0), 'file://'+names_in[i]+chr(0)) for i in range(jp2num)]
-        jp2box.DataReferenceBox(data_entry_url_boxes=url_).write(jpx)
+        url_ = jp2box.DataEntryURLBox(0, (0, 0, 0), None)
+
+        orig_pos = jpx.tell()
+        jpx.write(struct.pack('>I4sH', 0, b'dtbl', jp2num))
+
+        for i in range(jp2num):
+            url_.url = 'file://'+names_in[i]+chr(0)
+            url_.write(jpx)
+
+        end_pos = jpx.tell()
+        jpx.seek(orig_pos)
+        jpx.write(struct.pack('>I', end_pos - orig_pos))
+        jpx.seek(end_pos)
 
     with open(jpxname, 'wb') as ofile:
         ofile.write(jpx.getvalue())
