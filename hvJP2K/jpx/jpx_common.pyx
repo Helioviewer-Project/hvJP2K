93ba130a3f4b00786a14fd43b8cfc99cf1c49c2f
Bogdan Nicula <bogdan@nicula.net>
2014-05-07 13:02:21 +0000

    data: hvJP2K - add cythonization of jpx_common; up to 5.5x faster than kdu_merge
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/data/jpeg2000/hvJP2K@1076 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ hvJP2K/jpx/jpx_common.pyx
@@ -0,0 +1,186 @@
+
+import os
+import struct
+import warnings
+
+from glymur.jp2box import Jp2kBox, _BOX_WITH_ID, UnknownBox
+from glymur.codestream import Codestream
+
+from libc.stdint cimport uint32_t, uint64_t
+cdef extern from "arpa/inet.h":
+    uint32_t ntohl(uint32_t)
+
+cdef hv_parse_this_box(fptr, bytes box_id, uint32_t start, uint32_t num_bytes):
+    try:
+        parser = _BOX_WITH_ID[box_id].parse
+    except KeyError:
+        try:
+            parser = _BOX_WITH_ID[box_id].parse
+        except KeyError:
+            # We don't recognize the box ID, so create an UnknownBox and be
+            # done with it.
+            msg = 'Unrecognized box ({0}) encountered.'.format(box_id)
+            warnings.warn(msg)
+            return UnknownBox(box_id, offset=start, length=num_bytes)
+
+    try:
+        box = parser(fptr, start, num_bytes)
+    except ValueError as err:
+        msg = "Encountered an unrecoverable ValueError while parsing a {0} "
+        msg += "box at byte offset {1}.  The original error message was "
+        msg += "\"{2}\""
+        msg = msg.format(box_id.decode('utf-8'), start, str(err))
+        warnings.warn(msg, UserWarning)
+        box = UnknownBox(box_id.decode('utf-8'), length=num_bytes, offset=start)
+
+    return box
+
+
+cpdef hv_parse_superbox(fptr, uint32_t offset, uint32_t length):
+
+    superbox = []
+
+    cdef uint32_t box_length, num_bytes, cur_pos, start
+    cdef bytes read_buffer, box_id
+
+    start = fptr.tell()
+
+    while True:
+
+        # Are we at the end of the superbox?
+        if start >= offset + length:
+            break
+
+        read_buffer = fptr.read(8)
+        if len(read_buffer) < 8:
+            msg = "Extra bytes at end of file ignored."
+            warnings.warn(msg)
+            return superbox
+
+        (box_length, box_id) = struct.unpack('>I4s', read_buffer)
+
+        if box_length == 0:
+            # The length of the box is presumed to last until the end of
+            # the file.  Compute the effective length of the box.
+            # num_bytes = os.path.getsize(fptr.name) - fptr.tell() + 8
+            num_bytes = length - start # length + 8 - (start + 8)
+
+        elif box_length == 1:
+            # The length of the box is in the XL field, a 64-bit value.
+            read_buffer = fptr.read(8)
+            num_bytes, = struct.unpack('>Q', read_buffer)
+        else:
+            # The box_length value really is the length of the box!
+            num_bytes = box_length
+
+        superbox.append(hv_parse_this_box(fptr, box_id, start, num_bytes))
+
+        # Position to the start of the next box.
+        start += num_bytes
+        cur_pos = fptr.tell()
+
+        if num_bytes > length:
+            # Length of the current box goes past the end of the
+            # enclosing superbox.
+            msg = '{0} box has incorrect box length ({1})'
+            msg = msg.format(box_id, num_bytes)
+            warnings.warn(msg)
+        elif cur_pos > start:
+            # The box must be invalid somehow, as the file pointer is
+            # positioned past the end of the box.
+            msg = '{0} box may be invalid, the file pointer is positioned '
+            msg += '{1} bytes past the end of the box.'
+            msg = msg.format(box_id, cur_pos - start)
+            warnings.warn(msg)
+
+        fptr.seek(start)
+
+    return superbox
+
+# singleton essentially
+cdef class hvJPEG2000SignatureBox(object):
+    box_id = 'jP  '
+    @classmethod
+    def parse(cls, fptr, offset, length):
+        if fptr.read(4) != b'\x0D\x0A\x87\x0A':
+            print('JP2 signature verification failed: ' + fptr.name)
+            return None
+        return cls
+
+
+# singleton essentially
+cdef class hvFileTypeBox(object):
+    box_id = 'ftyp'
+    @classmethod
+    def parse(cls, fptr, offset, length):
+        if fptr.read(length - 8) != b'\x6A\x70\x32\x20\x00\x00\x00\x00\x6A\x70\x32\x20':
+            print('JP2 file type verification failed: ' + fptr.name)
+            return None
+        return cls
+
+
+cdef class hvJP2HeaderBox(object):
+    cdef public const char *box_id
+    cdef public uint32_t offset
+    cdef public uint32_t length
+    cdef public bytes header
+
+    def __init__(self, header, length, offset):
+        self.box_id = 'jp2h'
+        self.offset = offset
+        self.length = length
+        self.header = header
+
+    @classmethod
+    def parse(cls, fptr, offset, length):
+        # grab entire box
+        fptr.seek(offset)
+        return cls(fptr.read(length), length, offset)
+
+    def hv_parse(self, fptr):
+        fptr.seek(self.offset + 8)
+        return hv_parse_superbox(fptr, self.offset, self.length)
+
+
+cdef class hvXMLBox(object):
+    cdef public const char *box_id
+    cdef public uint32_t offset
+    cdef public uint32_t length
+    cdef public bytes xmlbuf
+
+    def __init__(self, xmlbuf, length, offset):
+        self.box_id = 'xml '
+        self.offset = offset
+        self.length = length
+        self.xmlbuf = xmlbuf
+
+    @classmethod
+    def parse(cls, fptr, offset, length):
+        # grab entire box
+        fptr.seek(offset)
+        return cls(fptr.read(length), length, offset)
+
+
+cdef class hvContiguousCodestreamBox(object):
+    cdef public const char *box_id
+    cdef public uint32_t offset
+    cdef public uint32_t length
+
+    def __init__(self, length, offset):
+        self.box_id = 'jp2c'
+        self.offset = offset
+        self.length = length
+
+    @classmethod
+    def parse(cls, fptr, offset, length):
+        main_header_offset = fptr.tell()
+        return cls(length + offset - main_header_offset, main_header_offset)
+
+    def hv_copy(self, ifile, ofile):
+        ifile.seek(self.offset)
+        ofile.write(struct.pack('>I4s', self.length + 8, b'jp2c'))
+        ofile.write(ifile.read(self.length))
+
+    def hv_parse(self, fptr):
+        fptr.seek(self.offset)
+        return Codestream(fptr, self.length, header_only=True)
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from Cython.Build import cythonize
 
 kwargs = {'name': 'hvJP2K',
           'description': 'JPEG2000 tools for the Helioviewer project',
@@ -6,12 +7,13 @@ kwargs = {'name': 'hvJP2K',
           'author': 'SWHV OMA',
           'author_email': 'swhv@oma.be',
           'url': 'https://github.com/helioviewer/...',
+          'ext_modules': cythonize('hvJP2K/jpx/jpx_common.pyx'),
           'packages': ['hvJP2K', 'hvJP2K.jp2', 'hvJP2K.jp2.data', 'hvJP2K.jpx'],
           'package_data': {'hvJP2K.jp2': ['data/*.sch', 'test/*.jp2', 'test/*.ppm'], 'hvJP2K.jpx': ['test/*/*.jp2', 'test/*/*.jpx']},
           'scripts': ['bin/hv_jp2_decode', 'bin/hv_jp2_encode', 'bin/hv_jp2_verify', 'bin/hv_jpx_merge', 'bin/hv_jpx_merged', 'bin/hv_jpx_split'],
           'license': 'MIT'}
 
-instllrqrs = ['numpy', 'lxml>=2.3', 'pillow', 'glymur>=0.5.10']
+instllrqrs = ['cython', 'numpy', 'lxml>=2.3', 'pillow', 'glymur>=0.5.10']
 # jpylyzer>=1.11.2 - doesn't work
 kwargs['install_requires'] = instllrqrs
 
