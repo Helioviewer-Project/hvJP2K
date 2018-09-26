7a99523c9c82b734db3af968df38425075266df6
Bogdan Nicula <bogdan@nicula.net>
2018-09-26 12:57:35 +0200

    Upgrade pip

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 dir=~/hvJP2K
 
 python -m venv ${dir}
-${dir}/bin/pip install cython
+${dir}/bin/pip install --upgrade pip
+${dir}/bin/pip install --upgrade cython
 ${dir}/bin/pip install --upgrade .
 
 cc -O2 bin/hv_jpx_mergec.c -o ${dir}/bin/hv_jpx_mergec
