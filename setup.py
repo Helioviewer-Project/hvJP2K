b1706c390880a549b74456365c162cb4b7a82830
Bogdan Nicula <bogdan@nicula.net>
2026-09-13 20:23:35 +0200

    Modernize packaging more

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 
 from Cython.Build import cythonize
 from setuptools import setup
+
 from distutils.ccompiler import new_compiler
 from distutils.command.build_scripts import build_scripts
 from distutils.sysconfig import customize_compiler
@@ -22,20 +23,7 @@ class BuildScripts(build_scripts):
         return super().get_source_files() + ["bin/hv_jpx_mergec.c"]
 
 
-with open("README.md", encoding="utf-8") as readme:
-    long_description = readme.read()
-
 setup(
-    name="hvJP2K",
-    version="0.6",
-    description="JPEG2000 tools for the Helioviewer Project",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="SWHV ROB",
-    author_email="swhv@oma.be",
-    url="https://github.com/Helioviewer-Project/hvJP2K",
-    license="MIT",
-    python_requires=">=3.11",
     ext_modules=cythonize(
         [
             "hvJP2K/jpx/jpx_common.pyx",
@@ -44,8 +32,6 @@ setup(
         build_dir="build/cython",
         language_level=3,
     ),
-    packages=["hvJP2K", "hvJP2K.jp2", "hvJP2K.jp2.data", "hvJP2K.jpx"],
-    package_data={"hvJP2K.jp2": ["data/*.sch"]},
     cmdclass={"build_scripts": BuildScripts},
     scripts=[
         "bin/hv_jp2_decode",
@@ -55,23 +41,5 @@ setup(
         "bin/hv_jpx_merged",
         "bin/hv_jpx_split",
         "bin/hv_jp2_transcode",
-    ],
-    install_requires=[
-        "astropy>=6.0",
-        "glymur>=0.14.2",
-        "jpylyzer==2.2.1",
-        "lxml>=5.0",
-        "numpy>=1.26",
-        "pillow>=10.0",
-    ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Science/Research",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
